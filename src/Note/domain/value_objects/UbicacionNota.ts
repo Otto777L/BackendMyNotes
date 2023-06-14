5ba89a53f1b790f7f2d8181677da42839f0a3740
Luis Guevara <74473129+TheLagger702@users.noreply.github.com>
2023-06-13 20:34:13 -0400

    Creacion de Ubicacion Nota
    - concretizacion del value object UbicionNota
    - Cambio entre las carpetas de la nota

--- src/Note/domain/value_objects/UbicacionNota.ts
+++ src/Note/domain/value_objects/UbicacionNota.ts
@@ -1,7 +1,26 @@
 import { IValueObject } from "src/core/domain/IValueObject";
 
-export abstract class UbicacionNota{
-    constructor(){}
+export class UbicacionNota implements IValueObject{
+    private latitud:number;
+    private altitud:number;
 
-    public abstract equals(other:IValueObject):boolean;
+    constructor(l:number, a:number){
+        this.latitud = l;
+        this.altitud = a;
+    }
+
+    public getLatitud():number{
+        return this.latitud;
+    }
+
+    public getAltitud():number{
+        return this.altitud;
+    }
+
+    public equals(other:UbicacionNota):boolean{
+        if((other.getAltitud() == this.altitud) && (other.getLatitud() == this.latitud))
+            return true;
+        else
+            return false;
+    }
 }
