6787dc78651a359acbdb9b68668f1cb7cdf3747c
alexis <sixela.quiros@gmail.com>
2023-06-14 11:55:37 -0400

    user Repo

---
+++ src/User/application/UserUseCase.ts
@@ -0,0 +1,12 @@
+import { UserRepository } from "../domain/repository/user.repository"
+
+export class UsertUsecase{
+    constructor(private UserRepository: UserRepository){}
+
+public findProductName=async (email: string ,pass :string)=>{
+  //  const product= new ProductValue(name)
+    const productFind = this.UserRepository.findUserLogin(email,pass)
+    return productFind
+}
+
+}
--- src/User/domain/repository/user.repository.ts
+++ src/User/domain/repository/user.repository.ts
@@ -1,8 +1,8 @@
-import { Optional } from "src/core/OrtogonalSolutions/Optional";
-import { User } from "./User";
+import { Optional } from "src/core/ortogonal_solutions/Optional";
+import { User } from "../User";
 
-export interface ProductRepository{
-    findProductname(email):Promise<Optional<User>>;
+export interface UserRepository{
+    findUserLogin(email:string,pass:string):Promise<Optional<User>>;
 
 }
 
